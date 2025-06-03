d9581d32d133b7693c9a6830974a12383f5e687f
kirav <kiravillagracia@yahoo.com>
2025-06-03 23:28:34 +0800

    added di modules

---
+++ app/src/main/java/com/kira/android/notescompose/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.kira.android.notescompose.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideNoteRepository(
+        noteRemoteSource: NoteRemoteSource
+    ) = provideNoteRepository(remoteSource = noteRemoteSource)
+}
