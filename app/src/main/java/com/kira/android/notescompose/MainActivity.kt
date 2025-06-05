c9731515e78d4f7c69f772ddfffc785b02ef8742
k.i.r.a. <kiravillagracia@yahoo.com>
2025-06-05 17:07:57 +0800

    added fab

--- app/src/main/java/com/kira/android/notescompose/MainActivity.kt
+++ app/src/main/java/com/kira/android/notescompose/MainActivity.kt
@@ -11,11 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.graphics.toColorInt
 import androidx.navigation.compose.rememberNavController
 import com.kira.android.notescompose.features.notes.list.NoteListScreen
 import com.kira.android.notescompose.ui.theme.NotesComposeTheme
@@ -38,7 +45,15 @@ fun MainScreenView() {
     val navController = rememberNavController()
     Scaffold(
         modifier = Modifier.safeDrawingPadding(),
-        contentWindowInsets = WindowInsets(0,0,0,0)
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = Color("#FFA500".toColorInt())
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Note")
+            }
+        }
     ) { contentPadding ->
         Box(modifier = Modifier
             .padding(contentPadding)
--- app/src/main/java/com/kira/android/notescompose/features/notes/list/NoteListScreen.kt
+++ app/src/main/java/com/kira/android/notescompose/features/notes/list/NoteListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -86,7 +87,6 @@ fun PopulateNoteListScreen(list: List<NoteResult>) {
             }
         }
     }
-
 }
 
 @Composable
