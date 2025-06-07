25c857d23aecae673597ef1c186d472b53609fa1
kirav <kiravillagracia@yahoo.com>
2025-06-07 16:29:09 +0800

    added edit and remove icons

--- app/src/main/java/com/kira/android/notescompose/features/notes/list/NoteListScreen.kt
+++ app/src/main/java/com/kira/android/notescompose/features/notes/list/NoteListScreen.kt
@@ -1,16 +1,27 @@
 package com.kira.android.notescompose.features.notes.list
 
+import android.graphics.Paint.Align
 import android.graphics.Typeface
 import android.util.Log
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +29,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -83,14 +95,24 @@ fun PopulateNoteListScreen(list: List<NoteResult>) {
         )
         LazyColumn(modifier = Modifier.fillMaxSize()) {
             items(list) { note ->
-                NoteItem(note)
+                NoteItem(
+                    note,
+                    { },
+                    { },
+                    { }
+                )
             }
         }
     }
 }
 
 @Composable
-fun NoteItem(note: NoteResult) {
+fun NoteItem(
+    note: NoteResult,
+    onItemClicked: (String) -> Unit,
+    onEditClick: (String) -> Unit,
+    onDeleteClick: (String) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,15 +123,48 @@ fun NoteItem(note: NoteResult) {
                 shape = RoundedCornerShape(11.dp)
             )
             .padding(16.dp)
+            .clickable { onItemClicked.invoke(note.id) },
     ) {
-        Text(
-            text = note.title,
-            style = TextStyle(
-                fontSize = 14.sp,
-                color = Color("#FFA500".toColorInt()),
-                fontFamily = FontFamily(typeface = Typeface.DEFAULT_BOLD)
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = note.title,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    color = Color("#FFA500".toColorInt()),
+                    fontFamily = FontFamily(typeface = Typeface.DEFAULT_BOLD)
+                )
             )
-        )
+
+            Row {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit",
+                        tint = Color("#FFA500".toColorInt())
+                    )
+                }
+
+                IconButton(
+                    onClick = {}, modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete",
+                        tint = Color("#FF0000".toColorInt())
+                    )
+                }
+            }
+        }
+
         Text(
             modifier = Modifier.padding(top = 10.dp),
             style = TextStyle(
